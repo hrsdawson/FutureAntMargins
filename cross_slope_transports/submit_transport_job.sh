e646448d6315e9f7d74edacd6a52f93398a0ec26
Hannah Dawson <62230733+hrsdawson@users.noreply.github.com>
2024-01-10 14:32:27 +1100

    submit_transport_job.sh

--- cross_slope_transports/submit_transport_job.sh
+++ cross_slope_transports/submit_transport_job.sh
@@ -18,13 +18,14 @@
 ## I/O filenames
 # this reads the name of the current run directory to use for output etc:
 #script_dir=/home/157/akm157/data_processing/cross_slope_transports/
-script_dir=/home/561/hd4873/project3/cross_slope_transport/
+script_dir=/home/561/hd4873/project3/cross_slope_transports/
 cd $script_dir
 #output_dir=/g/data/v45/akm157/model_data/access-om2/01deg_jra55v140_iaf_cycle3/vhrho_binned/
 
 # load conda
 module use /g/data3/hh5/public/modules
-module load conda/analysis3-23.04
+module load conda/analysis3-22.01
+#module load conda/analysis3
 
 # run
 python3 save_transports_along_contour.py $month $year &>> output_${month}.out
